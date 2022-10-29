e88e057a9190efc8354d938ac4d2f0df92600ba4
Miguel del Arco <migueldemollet10@gmail.com>
2022-10-29 18:04:09 +0200

    Adding aditional test in task

--- Test/task_controller_test.py
+++ Test/task_controller_test.py
@@ -24,10 +24,25 @@ class TestTaskController(unittest.TestCase):
         self.assertEqual(self.controller.add_task(task), True)
         self.assertEqual(self.controller.add_task(task), 0)
 
+    #-------------------------Name-------------------------------
+
     def test_change_name(self):
         self.assertEqual(self.controller.change_name('task1', 'task4'), True)
         self.assertEqual(self.controller.change_name('task4', 'task4'), 1)
         self.assertEqual(self.controller.change_name('task1', 'task7'), 0)
+
+    def test_delete_by_name(self):
+        self.assertEqual(self.controller.delete_by_name('task1'), True)
+        self.assertEqual(self.controller.delete_by_name('task1'), 0)
+        self.assertEqual(self.controller.delete_by_name('task1xx'), 0)
+
+    #-------------------------Status-------------------------------
+
+    def test_get_by_status(self):
+        self.assertEqual(self.controller.get_by_status(0), [{'name': 'task1', 'status': 0, 'tag': 1, 'date': '01/01/2022', 'color': 'red', 'priority': 1}])
+        self.assertEqual(self.controller.get_by_status(1), [{'name': 'task2', 'status': 1, 'tag': 2, 'date': '01/02/2022', 'color': 'blue', 'priority': 2}])
+        self.assertEqual(self.controller.get_by_status(2), 1)
+        self.assertEqual(self.controller.get_by_status(-1), 1)
 
     def test_change_status(self):
         task = Task('task1', 1, '01/01/2022', 'red', 1)
@@ -39,6 +54,22 @@ class TestTaskController(unittest.TestCase):
         self.assertEqual(self.controller.change_status(task, 2), 1)
         self.assertEqual(self.controller.change_status(task, -1), 1)
         self.assertEqual(self.controller.change_status(task_invented, 1), 0)
+
+    def test_delete_by_status(self):
+        self.assertEqual(self.controller.delete_by_status(0), True)
+        self.assertEqual(self.controller.delete_by_status(0), 0)
+        self.assertEqual(self.controller.delete_by_status(1), True)
+        self.assertEqual(self.controller.delete_by_status(2), 1)
+        self.assertEqual(self.controller.delete_by_status(-1), 1)
+
+    #-------------------------Tag-------------------------------
+
+    def test_get_by_tag(self):
+        self.assertEqual(self.controller.get_by_tag(1), [{'name': 'task1', 'status': 0, 'tag': 1, 'date': '01/01/2022', 'color': 'red', 'priority': 1}])
+        self.assertEqual(self.controller.get_by_tag(2), [{'name': 'task2', 'status': 1, 'tag': 2, 'date': '01/02/2022', 'color': 'blue', 'priority': 2}])
+        self.assertEqual(self.controller.get_by_tag(3), 0)
+        self.assertEqual(self.controller.get_by_tag(-1), 0)
+        self.assertEqual(self.controller.get_by_tag("sdsd"), 1)
     
     def test_change_tag(self):
         task = Task('task1', 1, '01/01/2022', 'red', 1)
@@ -47,6 +78,21 @@ class TestTaskController(unittest.TestCase):
         self.assertEqual(self.controller.change_tag(task, '2'), 1)
         self.assertEqual(self.controller.change_tag(task, '3'), 0)
         self.assertEqual(self.controller.change_tag(task_invented, '2'), 0)
+
+    def test_delete_by_tag(self):
+        self.assertEqual(self.controller.delete_by_tag(1), True)
+        self.assertEqual(self.controller.delete_by_tag(0), 1)
+        self.assertEqual(self.controller.delete_by_tag(5), 0)
+        self.assertEqual(self.controller.delete_by_tag("dfd"), 1)
+
+    #-------------------------Date-------------------------------
+
+    def test_get_by_date(self):
+        self.assertEqual(self.controller.get_by_date('01/01/2022'), [{'name': 'task1', 'status': 0, 'tag': 1, 'date': '01/01/2022', 'color': 'red', 'priority': 1}])
+        self.assertEqual(self.controller.get_by_date('01/02/2022'), [{'name': 'task2', 'status': 1, 'tag': 2, 'date': '01/02/2022', 'color': 'blue', 'priority': 2}])
+        self.assertEqual(self.controller.get_by_date('01/03/2022'), [])
+        self.assertEqual(self.controller.get_by_date('01/0sd4/sa2022'), 1)
+        self.assertEqual(self.controller.get_by_date('51/04/022'), 1)
 
     def test_change_date(self):
         task = Task('task1', 1, '01/01/2022', 'red', 1)
@@ -57,6 +103,20 @@ class TestTaskController(unittest.TestCase):
         self.assertEqual(self.controller.change_date(task, '45/-2/e3'), 1)
         self.assertEqual(self.controller.change_date(task_invented, '01/04/2022'), 0)
     
+    def test_delete_by_date(self):
+        self.assertEqual(self.controller.delete_by_date('01/01/2022'), True)
+        self.assertEqual(self.controller.delete_by_date('01/01/2022'), 0)
+        self.assertEqual(self.controller.delete_by_date('111/32/2022'), 1)
+
+    #-------------------------Color-------------------------------
+
+    def test_get_by_color(self):
+        self.assertEqual(self.controller.get_by_color('red'), [{'name': 'task1', 'status': 0, 'tag': 1, 'date': '01/01/2022', 'color': 'red', 'priority': 1}])
+        self.assertEqual(self.controller.get_by_color('blue'), [{'name': 'task2', 'status': 1, 'tag': 2, 'date': '01/02/2022', 'color': 'blue', 'priority': 2}])
+        self.assertEqual(self.controller.get_by_color('green'), [])
+        self.assertEqual(self.controller.get_by_color('red1'), 1)
+        self.assertEqual(self.controller.get_by_color('dfdfd'), 1)
+    
     def test_change_color(self):
         task = Task('task1', 1, '01/01/2022', 'red', 1)
         task_invented = Task('fdfdf', 1, '01/01/2022', 'red', 1)
@@ -64,6 +124,22 @@ class TestTaskController(unittest.TestCase):
         self.assertEqual(self.controller.change_color(task, 'blue'), 1)
         self.assertEqual(self.controller.change_color(task, 'left'), 1)
         self.assertEqual(self.controller.change_color(task_invented, 'blue'), 0)
+
+    def test_delete_by_color(self):
+        self.assertEqual(self.controller.delete_by_color('red'), True)
+        self.assertEqual(self.controller.delete_by_color('red'), 0)
+        self.assertEqual(self.controller.delete_by_color('blue'), True)
+        self.assertEqual(self.controller.delete_by_color('blue'), 0)
+        self.assertEqual(self.controller.delete_by_color('green'), 1)
+
+    #-------------------------Priority-------------------------------
+
+    def test_get_by_priority(self):
+        self.assertEqual(self.controller.get_by_priority(1), [{'name': 'task1', 'status': 0, 'tag': 1, 'date': '01/01/2022', 'color': 'red', 'priority': 1}])
+        self.assertEqual(self.controller.get_by_priority(2), [{'name': 'task2', 'status': 1, 'tag': 2, 'date': '01/02/2022', 'color': 'blue', 'priority': 2}])
+        self.assertEqual(self.controller.get_by_priority(3), 1)
+        self.assertEqual(self.controller.get_by_priority(0), [])
+        self.assertEqual(self.controller.get_by_priority(4), 1)
 
     def test_change_priority(self):
         task = Task('task1', 1, '01/01/2022', 'red', 1)
@@ -76,30 +152,12 @@ class TestTaskController(unittest.TestCase):
         self.assertEqual(self.controller.change_priority(task, -1), 1)
         self.assertEqual(self.controller.change_priority(task_invented, 2), 0)
 
-    def test_delete_by_name(self):
-        self.assertEqual(self.controller.delete_by_name('task1'), True)
-        self.assertEqual(self.controller.delete_by_name('task1'), 0)
-        self.assertEqual(self.controller.delete_by_name('task1xx'), 0)
-
-    def test_delete_by_status(self):
-        self.assertEqual(self.controller.delete_by_status(0), True)
-        self.assertEqual(self.controller.delete_by_status(0), 0)
-        restore()
-        self.assertEqual(self.controller.delete_by_status(1), True)
-        restore()
-        self.assertEqual(self.controller.delete_by_status(2), 1)
-        self.assertEqual(self.controller.delete_by_status(-1), 1)
-
-    def test_delete_by_tag(self):
-        self.assertEqual(self.controller.delete_by_tag(1), True)
-        self.assertEqual(self.controller.delete_by_tag(0), 1)
-        self.assertEqual(self.controller.delete_by_tag(5), 0)
-        self.assertEqual(self.controller.delete_by_tag("dfd"), 1)
-
-    def test_delete_by_date(self):
-        self.assertEqual(self.controller.delete_by_date('01/01/2022'), True)
-        self.assertEqual(self.controller.delete_by_date('01/01/2022'), 0)
-        self.assertEqual(self.controller.delete_by_date('111/32/2022'), 1)
+    def test_delete_by_priority(self):
+        self.assertEqual(self.controller.delete_by_priority(1), True)
+        self.assertEqual(self.controller.delete_by_priority(1), 0)
+        self.assertEqual(self.controller.delete_by_priority(2), True)
+        self.assertEqual(self.controller.delete_by_priority(2), 0)
+        self.assertEqual(self.controller.delete_by_priority(3), 1)
 
 
 def restore():
