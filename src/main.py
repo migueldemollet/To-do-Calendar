2d3a98e37ac56bdd67f05c02d606b3ed5f812d49
totoro2012adri <adrianrodriguezfajardo@gmail.com>
2022-10-30 14:31:33 +0100

    raw calendar with text under numbers

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from View.calendar_view import *
+
+if __name__ == '__main__':
+    import tkinter as tk
+    root = tk.Tk()
+    root.geometry("800x500")
+    agenda = Agenda(root, selectmode='day')
+    date = agenda.datetime.today()# + agenda.timedelta(days=2)
+    agenda.calevent_create(date, 'Hello World', 'message')
+    agenda.calevent_create(date, 'Reminder 2', 'reminder')
+    agenda.calevent_create(date + agenda.timedelta(days=-7), 'Reminder 1', 'reminder')
+    agenda.calevent_create(date + agenda.timedelta(days=3), 'Message', 'message')
+    agenda.calevent_create(date + agenda.timedelta(days=3), 'Another message', 'message')
+    agenda.calevent_remove(date + agenda.timedelta(days=3))
+
+    agenda.tag_config('reminder', background='red', foreground='white')
+
+    agenda.pack(fill="both", expand=True)
+    
+    labelsito = tk.Label(text='hey')
+    labelsito.pack()
+    root.mainloop()    
