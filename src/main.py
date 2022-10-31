1d12d7dde820d27955d25d67195f4ab41d0f2ce0
Adrián Rodríguez <80252727+es2021uab-1428612@users.noreply.github.com>
2022-10-31 12:01:42 +0100

    Update main.py
    Pruebas con el main

--- src/main.py
+++ src/main.py
@@ -4,16 +4,19 @@ if __name__ == '__main__':
     import tkinter as tk
     root = tk.Tk()
     root.geometry("800x500")
+    new_frame = tk.Frame(root)
     agenda = Agenda(root, selectmode='day')
     date = agenda.datetime.today()# + agenda.timedelta(days=2)
     agenda.calevent_create(date, 'Hello World', 'message')
-    agenda.calevent_create(date, 'Reminder 2', 'reminder')
+    agenda.calevent_create(date, 'Reminder 2', 'message')
     agenda.calevent_create(date + agenda.timedelta(days=-7), 'Reminder 1', 'reminder')
     agenda.calevent_create(date + agenda.timedelta(days=3), 'Message', 'message')
-    agenda.calevent_create(date + agenda.timedelta(days=3), 'Another message', 'message')
-    agenda.calevent_remove(date + agenda.timedelta(days=3))
+    agenda.calevent_create(date + agenda.timedelta(days=3), 'Another message', 'reminder')
+    #agenda.calevent_remove(date + agenda.timedelta(days=3))
 
     agenda.tag_config('reminder', background='red', foreground='white')
+    agenda.tag_config('message', background='blue', foreground='white')
+    agenda.tag_config('mix',background='grey',foreground='white')
 
     agenda.pack(fill="both", expand=True)
     
