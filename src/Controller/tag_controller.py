f847228703f16ffa714a82c514b3f7b41a364849
Miguel del Arco <migueldemollet10@gmail.com>
2022-10-31 19:14:11 +0100

    Adapting tag to new db

--- src/Controller/tag_controller.py
+++ src/Controller/tag_controller.py
@@ -10,74 +10,104 @@ class TagController:
         self.tag_model = TagModel("./DB/to_do_calendar_test.db")
 
     def add(self, tag):
-        if(self.tag_model.get_by_name(tag.name) == []):
+        if (not check_is_int(tag.user.id)):
+            print("Invalid user id")
+            return 1
+        if (not check_color(tag.color)):
+            print("Invalid color")
+            return 1
+        if(self.tag_model.get_by_id(tag.id) == []):
             self.tag_model.add(tag)
             return True
         else:
             print("Tag already exists")
             return 0
 
-    def get_all(self):
-        tags = self.tag_model.get_all()
-        return tags
+    def get_all(self, user_id):
+        if (not check_is_int(user_id)):
+            print("Invalid user id")
+            return 1
+        return self.tag_model.get_all(user_id)
     
-    def delete_all(self):
-        return self.tag_model.delete_all()
+    def delete_all(self, user_id):
+        if (not check_is_int(user_id)):
+            print("Invalid user id")
+            return 1
+        return self.tag_model.delete_all(user_id)
 
     #-------------------------Name-------------------------------
 
-    def get_by_name(self, name):
-        return self.tag_model.get_by_name(name)
+    def get_by_name(self, name, user_id):
+        if (not check_is_int(user_id)):
+            print("Invalid user id")
+            return 1
+        return self.tag_model.get_by_name(name, user_id)
 
-    def change_name(self, name, new_name):
+    def change_name(self, name, new_name, user_id):
+        if (not check_is_int(user_id)):
+            print("Invalid user id")
+            return 1
         if(name == new_name):
             print("Tag already has this name")
             return 1
 
-        if(self.tag_model.get_by_name(name) != []):
-            return self.tag_model.change_name(name, new_name)
+        if(self.tag_model.get_by_name(name, user_id) != []):
+            return self.tag_model.change_name(name, new_name, user_id)
         else:
             print("Tag "+name+" does not exist")
             return 0
     
-    def delete_by_name(self, name):
-        if(self.tag_model.get_by_name(name) != []):
-            return self.tag_model.delete_by_name(name)
+    def delete_by_name(self, name, user_id):
+        if (not check_is_int(user_id)):
+            print("Invalid user id")
+            return 1
+        if(self.tag_model.get_by_name(name, user_id) != []):
+            return self.tag_model.delete_by_name(name, user_id)
         else:
             print("Tag "+name+" does not exist")
             return 0
 
     #-------------------------Color-------------------------------
     
-    def get_by_color(self, color):
+    def get_by_color(self, color, user_id):
         if (not check_color(color)):
             print("Invalid color")
             return 1
-
-        return self.tag_model.get_by_color(color)
+        if (not check_is_int(user_id)):
+            print("Invalid user id")
+            return 1
+        return self.tag_model.get_by_color(color, user_id)
     
     def change_color(self, tag, new_color):
         if (not check_color(new_color)):
             print("Invalid color")
             return 1
 
+        if (not check_is_int(tag.user.id)):
+            print("Invalid user id")
+            return 1
+
         if (tag.color == new_color):
             print("Tag already has this color")
             return 1
         
-        if(self.tag_model.get_by_name(tag.name) != []):
-            return self.tag_model.change_color(tag.name, new_color)
+        if(self.tag_model.get_by_id(tag.id) != []):
+            return self.tag_model.change_color(tag.id, new_color)
         else:
             print("Tag "+tag.name+" does not exist")
             return 0
     
-    def delete_by_color(self, color):
+    def delete_by_color(self, color, user_id):
         if (not check_color(color)):
             print("Invalid color")
             return 1
 
-        if(self.tag_model.get_by_color(color) != []):
-            return self.tag_model.delete_by_color(color)
+        if (not check_is_int(user_id)):
+            print("Invalid user id")
+            return 1
+
+        if(self.tag_model.get_by_color(color, user_id) != []):
+            return self.tag_model.delete_by_color(color, user_id)
         else:
             print("Tag with color "+color+" does not exist")
             return 0
